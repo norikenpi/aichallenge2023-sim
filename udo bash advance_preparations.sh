c143b3bf319e55226d1b814bc3870f0b2a53b971
norikenpi <norikenpi@gmail.com>
2023-08-26 13:10:37 +0900

    By adding road shoulder margin, it can avoid all obstacles

---
+++ udo bash advance_preparations.sh
@@ -0,0 +1,13 @@
+[1mdiff --git a/docker/aichallenge/aichallenge_ws/src/aichallenge_submit/aichallenge_submit_launch/launch/autoware_mini_awsim.launch.xml b/docker/aichallenge/aichallenge_ws/src/aichallenge_submit/aichallenge_submit_launch/launch/autoware_mini_awsim.launch.xml[m
+[1mindex f833523..51cf855 100644[m
+[1m--- a/docker/aichallenge/aichallenge_ws/src/aichallenge_submit/aichallenge_submit_launch/launch/autoware_mini_awsim.launch.xml[m
+[1m+++ b/docker/aichallenge/aichallenge_ws/src/aichallenge_submit/aichallenge_submit_launch/launch/autoware_mini_awsim.launch.xml[m
+[36m@@ -622,7 +622,7 @@[m
+       </group> <!-- lane_driving -->[m
+ [m
+     </group>[m
+[31m-[m
+[32m+[m[32m    <!-- planning validator -->[m
+   </group>[m
+ [m
+   <!-- Control -->[m
